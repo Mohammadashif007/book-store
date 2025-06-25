5a12001f32d1ae284ad877a5ad32f56b7f9169f7
Mohammad Ashif <mohammadashif510@gmail.com>
2025-06-25 16:43:32 +0600

    password hashed done and return password field empty string

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,10 +1,23 @@
 import { model, Schema } from "mongoose";
 import { TUser } from "./user.interface";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
 const userSchema = new Schema<TUser>({
   fullName: { type: String, required: [true, "FullName is required"] },
   email: { type: String, required: [true, "email is required"] },
   password: { type: String, required: [true, "Password is required"] },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, Number(config.salt_round));
+  next();
+});
+
+userSchema.post("save", function (doc, next) {
+  doc.password = "";
+  next();
 });
 
 export const User = model<TUser>("User", userSchema);
